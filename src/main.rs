00d5e418ffa3a1af79303c3a00582c8579435d41
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2023-09-26 17:21:01 +1000

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use nalgebra::{DVector, Vector2};
 use bno055::mint::{Quaternion,Vector3};
 use bno055::{Bno055,Error as BNOError};
+use embedded_hal::blocking::i2c::{WriteRead,Write};
+use rppal::{
+    hal::Delay,
+    i2c::{Error as I2cError, I2c},
+};
 
 const IMU_COUNT: usize = 3;
 const QUEUE_LENGTH: usize = 5;
@@ -31,60 +36,93 @@ impl Imu for BadImu {
     }
 }
 
-struct Bno055imu<I: WriteRead<Error = BNOError> + Write<Error = BNOError>> {
-    imu: Bno055<I>,
+struct Bno055imu {
+    imu: Bno055<I2c>,
+}
+
+impl Imu for Bno055imu
+{
+    fn reading(&self) -> Result<ImuReading, I2cError> {
+        let accel = self.imu.linear_acceleration_fixed();
+        let angaccel = self.
+
+        ImuReading {
+            accel,
+            veloc: self.vel + accel,
+            angacce
+        }
+    }
+}
+
+struct Bias {
     offset: Vector3<f32>,
     rotation: Quaternion<f32>,
 }
 
-// Output from IMU is likely mint::Vector3
-type Pose = Vector2<f32>;
-
 // Remove this ASAP. Will will kill me if he sees this.
 #[derive(Clone)]
 struct ImuReading {
-    accel: f32,
-    veloc: f32,
-    angaccel: f32,
-    angveloc: f32,
+    accel: Vector3<f32>,
+    veloc: Vector3<f32>,
+    angaccel: Vector3<f32>,
+    angveloc: Vector3<f32>,
 }
 
-struct Odom {
+struct VirtualImu {
     algorithm: DeadReckoningAlgorithms,
     // imus: Vec<imu> ,
-    imus: Vec<Box<dyn Imu>>,
+    imus: Vec<(Box<dyn Imu>, Bias)>,
 }
 
 
-impl Odom {
-    fn new(imus: Vec<Box<dyn Imu>>) -> Odom {
-        Odom {
+impl VirtualImu {
+    fn new(imus: Vec<(Box<dyn Imu>, Bias)>) -> VirtualImu {
+        VirtualImu {
             algorithm: DeadReckoningAlgorithms::default(),
             imus,
         }
     }
 
-    fn imu_integrate(&mut self) {
+    fn from_one(imu: Box<dyn Imu>, bias: Bias) -> VirtualImu {
+        VirtualImu {
+            algorithm: DeadReckoningAlgorithms::default(),
+            imus: vec![(imu, bias)],
+        }
+    }
+
+    fn imu_integrate(&mut self) -> Option<ImuReading> {
         // No measurement model, just dead reckoning
         let mut measurements: Vec<ImuReading> = self.imus.iter().map(|x| x.reading()).collect();
 
         // Virtual IMU reading
-        let vimu = match &self.algorithm {
+        match &self.algorithm {
             DeadReckoningAlgorithms::RollingAverage => rolling_average(measurements),
             DeadReckoningAlgorithms::KalmanFilter => unimplemented!()
-        };
+        }
     }
 
 }
+
+struct Robot {
+    posit: Vector2<f32>,
+    veloc: Vector2<f32>,
+    accel: Vector2<f32>,
+    angveloc: Vector2<f32>,
+}
+
+impl Robot {
+    fn best_guess() {}
+}
+
 
 // Takes a Vec for convenience right now. Consider making it an array
 // Assume that the period that the measurements are taken over are short
 // Returns a virtual IMU reading
 fn rolling_average(readings: Vec<ImuReading>) -> Option<ImuReading> {
     let mut init = DVector::zeros(2);
-    let readingsnum = readings.len() as f32;
+    let readingsnum = readings.len();
 
-    if readingsnum == 0. { return None };
+    if readingsnum == 0 { return None };
 
     readings.iter()
         .fold(&mut init, |a, x| {
@@ -92,7 +130,7 @@ fn rolling_average(readings: Vec<ImuReading>) -> Option<ImuReading> {
             a
         });
 
-    init /= readingsnum;
+    init /= readingsnum as f32;
 
     Some(ImuReading {
         veloc: init[0],
